a8b01e2f93e61d41c56a1820af12e18b3231aed7
Yeonseo-Jo <jys073190@gmail.com>
2023-10-27 14:44:45 +0900

    level 1 - 시저암호

---
+++ 연서/week3/level1/시저암호.js
@@ -0,0 +1,21 @@
+function solution(s, n) {
+  const answer = s
+    .split("")
+    .map((string) => {
+      const code = string.charCodeAt();
+      //소문자
+      if (97 <= code && code <= 122) {
+        return String.fromCharCode(((code + n - 97) % 26) + 97);
+      }
+
+      //대문자
+      if (65 <= code && code <= 90) {
+        return String.fromCharCode(((code + n - 65) % 26) + 65);
+      }
+
+      return " ";
+    })
+    .join("");
+
+  return answer;
+}
